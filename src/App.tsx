0a8069834ca8a536afac1b1f231b6ad976373850
Devansh Kaila <72502517+devansh-kaila-006@users.noreply.github.com>
2026-01-18 17:38:33 +0000

    Fix React Router basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import About from "./pages/About";
 import WhyChooseUs from "./pages/WhyChooseUs";
@@ -22,7 +23,9 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+
+      {/* 🔴 CRITICAL FIX FOR GITHUB PAGES */}
+      <BrowserRouter basename="/New-Akromind">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
