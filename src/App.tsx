fe4b9ebf49668f5aebb4e1b5f51377ee0540e113
Devansh Kaila <72502517+devansh-kaila-006@users.noreply.github.com>
2026-01-23 13:41:31 +0000

    Fix SPA routing for custom domain

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/New-Akromind">
+      <BrowserRouter>
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/about" element={<About />} />
